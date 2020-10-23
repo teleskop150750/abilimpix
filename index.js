23f89851bc61c19fe9abfc988cc623597e4fe044
teleskop150750 <teleskop150750@gmail.com>
2020-10-23 14:42:07 +0300

    new

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const btnNavClose = document.querySelector('.nav__close');
 const btnNavOpen = document.querySelector('.header__burger');
 const nav = document.querySelector('.nav');
+const { body } = document;
+
+const addPadding = () => {
+  const elements = document.querySelectorAll('.js-scroll');
+  elements.forEach((item) => {
+    const el = item;
+    const { paddingRight } = getComputedStyle(item);
+    el.style.paddingRight = `${parseFloat(paddingRight) + window.innerWidth - document.documentElement.clientWidth}px`;
+  });
+};
+
+const removePadding = () => {
+  const elements = document.querySelectorAll('.js-scroll');
+  elements.forEach((item) => {
+    const el = item;
+    const { paddingRight } = getComputedStyle(item);
+    el.style.paddingRight = `${parseFloat(paddingRight) - (window.innerWidth - document.documentElement.clientWidth)}px`;
+  });
+};
 
 const navOpen = () => {
+  addPadding();
+  body.classList.add('page__body--lock');
   nav.classList.add('nav--open');
 };
 
 const navClose = (e) => {
   if (!e.target.classList.contains('nav__list')) {
+    body.classList.remove('page__body--lock');
+    removePadding();
     nav.classList.remove('nav--open');
   }
 };
 
 btnNavOpen.addEventListener('click', navOpen);
-btnNavClose.addEventListener('click', navClose, true);
 nav.addEventListener('click', navClose);
